a368c35a89fca2ee7851d82dfceb7326cb95d2b3
Chris Shaw <shawch28@gmail.com>
2026-02-23 11:44:29 -0500

    Remove unused import for CurrentLimitsConfigs in ShooterSubsystem.java
    They're set through TalonFXConfiguration.CurrentLimits, which is the standard approach when we already are applying a full config

--- src/main/java/frc/robot/subsystems/shoot/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shoot/ShooterSubsystem.java
@@ -8,7 +8,6 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.controls.VelocityVoltage;
