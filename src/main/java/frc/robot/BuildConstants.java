e6104075a4cb1995ba2b4704c968ac5f63a443e7
UltraBots8041 <lcrobotics@lakecitytrojans.org>
2026-06-27 15:53:43 -0400

    ignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt_Bot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 59;
-  public static final String GIT_SHA = "a91eb9b3c1a54f959bad6ccaa92956e651f09cbd";
-  public static final String GIT_DATE = "2026-06-27 14:50:15 EDT";
+  public static final int GIT_REVISION = 60;
+  public static final String GIT_SHA = "1e170b308747d41679a640c71e7b678e37f353f5";
+  public static final String GIT_DATE = "2026-06-27 14:53:08 EDT";
   public static final String GIT_BRANCH = "feat/advantagekit";
-  public static final String BUILD_DATE = "2026-06-27 14:51:27 EDT";
-  public static final long BUILD_UNIX_TIME = 1782586287474L;
+  public static final String BUILD_DATE = "2026-06-27 15:53:33 EDT";
+  public static final long BUILD_UNIX_TIME = 1782590013118L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
